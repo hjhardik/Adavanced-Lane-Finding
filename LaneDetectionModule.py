6a4cde8f277c74f25a7a6989d65f5f742d516406
Hardik Joshi <hardikj7321@gmail.com>
2021-04-04 23:11:29 +0530

    added remaining steps code in lanedetect module

--- LaneDetectionModule.py
+++ LaneDetectionModule.py
@@ -6,20 +6,65 @@ import utils
 def getLaneCurve(img):
     # create a copy image to display warp points
     imgCopy = img.copy()
+    
     ##1 first find the threshold image
-    imgThres = utils.thresholding(img)
+        imgThres = utils.thresholding(img)
 
     ##2 now find the warped tracking coordinates
     h, w, c = img.shape
     points = utils.valTrackbars()
     imgWarp = utils.warpImg(imgThres, points, w, h)
     imgWarpPoints = utils.drawPoints(imgCopy, points)
+
+    # for displaying the resultant images from above, uncomment the below lines  
+    # cv2.imshow('Threshold', imgThres)
+    # cv2.imshow('Warp', imgWarp)
+    # cv2.imshow('Warp Points', imgWarpPoints)
+    # return None
     
-    # display the resultant images 
-    cv2.imshow('Threshold', imgThres)
-    cv2.imshow('Warp', imgWarp)
-    cv2.imshow('Warp Points', imgWarpPoints)
-    return None
+    ##3 now find 
+    middlePoint, imgHist = utils.getHistogram(imgWarp, display=True, minPer=0.5, region=4)
+    curveAveragePoint, imgHist = utils.getHistogram(imgWarp, display=True, minPer=0.9)
+    curveRaw = curveAveragePoint - middlePoint
+
+    #### SETP 4
+    curveList.append(curveRaw)
+    if len(curveList)>avgVal:
+        curveList.pop(0)
+    curve = int(sum(curveList)/len(curveList))
+ 
+    #### STEP 5
+    if display != 0:
+        imgInvWarp = utlis.warpImg(imgWarp, points, wT, hT, inv=True)
+        imgInvWarp = cv2.cvtColor(imgInvWarp, cv2.COLOR_GRAY2BGR)
+        imgInvWarp[0:hT // 3, 0:wT] = 0, 0, 0
+        imgLaneColor = np.zeros_like(img)
+        imgLaneColor[:] = 0, 255, 0
+        imgLaneColor = cv2.bitwise_and(imgInvWarp, imgLaneColor)
+        imgResult = cv2.addWeighted(imgResult, 1, imgLaneColor, 1, 0)
+        midY = 450
+        cv2.putText(imgResult, str(curve), (wT // 2 - 80, 85), cv2.FONT_HERSHEY_COMPLEX, 2, (255, 0, 255), 3)
+        cv2.line(imgResult, (wT // 2, midY), (wT // 2 + (curve * 3), midY), (255, 0, 255), 5)
+        cv2.line(imgResult, ((wT // 2 + (curve * 3)), midY - 25), (wT // 2 + (curve * 3), midY + 25), (0, 255, 0), 5)
+        for x in range(-30, 30):
+            w = wT // 20
+            cv2.line(imgResult, (w * x + int(curve // 50), midY - 10),
+                     (w * x + int(curve // 50), midY + 10), (0, 0, 255), 2)
+        #fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer);
+        #cv2.putText(imgResult, 'FPS ' + str(int(fps)), (20, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (230, 50, 50), 3);
+    if display == 2:
+        imgStacked = utlis.stackImages(0.7, ([img, imgWarpPoints, imgWarp],
+                                             [imgHist, imgLaneColor, imgResult]))
+        cv2.imshow('ImageStack', imgStacked)
+    elif display == 1:
+        cv2.imshow('Resutlt', imgResult)
+ 
+    #### NORMALIZATION
+    curve = curve/100
+    if curve>1: curve ==1
+    if curve<-1:curve == -1
+ 
+    return curve
 
 if __name__ == "__main__":
     # capture the training video of correct lane driving
