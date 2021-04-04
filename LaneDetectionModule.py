8fc39605368f54892495667ec4b3fcf74d80c881
Hardik Joshi <hardikj7321@gmail.com>
2021-04-04 15:21:49 +0530

    added while to continue looping after video is over

--- LaneDetectionModule.py
+++ LaneDetectionModule.py
@@ -4,13 +4,32 @@ import utils
 
 # finding the lane curves from the sampleTraining video
 def getLaneCurve(img):
+    ##1 first find the threshold image
     imgThres = utils.thresholding(img)
+
+    ##2 now find the warped tracking coordinates
+    h, w, c = img.shape
+    points = utils.valTrackbars()
+    imgWarp = utils.warpImg(img, points, w, h)
+    imgWarpPoints = utils.drawPoints(img, Points)
+    
+    # display the resultant images 
     cv2.imshow('Thres', imgThres)
-    return
+    cv2.imshow('Warp', imgWarp)
+    cv2.imshow('Warp Points', imgWarpPoints)
+    return None
 
 if __name__ == "__main__":
+    # capture the training video of correct lane driving
     cap = cv2.VideoCapture('sampleTraining.mp4')
+    frameCounter = 0
     while True:
+        frameCounter +=1
+        #keep repeating after video is over
+        if cap.get(cv2.CAP_PROP_FRAME_COUNT) == frameCounter:
+            cap.set(cv2.CAP_PROP_POS_FRAMES,0)
+            frameCounter=0
+
         _, img = cap.read() # GET THE IMAGE
         img = cv2.resize(img,(640,480)) # RESIZE
         getLaneCurve(img)
--- utils.py
+++ utils.py
@@ -14,11 +14,30 @@ def thresholding(img):
     return maskWhite
 
 # WARPING FUNCTION IMPLEMENTATION
-# trackbar change ill call nothing()
-def nothing(a):
+def warpImg (img,points,w,h,inv=False):
+
+    pts1 = np.float32(points)
+    # defining the border coordinates of the warped image
+    pts2 = np.float32([[0,0], [w,0], [0,h], [w,h]])
+    # finding the transformation matrix
+    if inv:
+        #if inverted interchange pts2 and pts1
+        matrix = cv2.getPerspectiveTransform(pts2,pts1)
+    else:
+        matrix = cv2.getPerspectiveTransform(pts1,pts2)
+    
+    imgWarp = cv2.warpPerspective(img,matrix,(w,h))
+    return imgWarp
+
+# trackbar change will call nothing()
+def nothing(a): 
     pass
 
-def initializeTrackbars(intialTracbarVals,wT=480, hT=240):
+# Creating the trackbars to find the optimal warping points.
+# Care should be taken to choose points which are not very far from our current position
+# ie. mostly lying in the bottom half region of the image since we should only confidently
+# predict the lane warp present on the road at this point of time.
+def initializeTrackbars(intialTracbarVals,wT=480, hT=240): #wT and hT are the weight and height targets resp.
     cv2.namedWindow("Trackbars")
     cv2.resizeWindow("Trackbars", 360, 240)
     cv2.createTrackbar("Width Top", "Trackbars", intialTracbarVals[0],wT//2, nothing)
