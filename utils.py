05384e874bd370539baf2aac2c0992fa7fb7a330
Hardik Joshi <hardikj7321@gmail.com>
2021-04-04 14:11:52 +0530

    adding warping function to utils

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 
+# THRESHOLDING FUNCTION
 def thresholding(img):
     # visualizing image in HSV parameters
     imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
@@ -11,3 +12,25 @@ def thresholding(img):
     # passing the values of lowerWhite and upperWhite to create the mask
     maskWhite = cv2.inRange(imgHSV, lowerWhite, upperWhite)
     return maskWhite
+
+# WARPING FUNCTION IMPLEMENTATION
+# trackbar change ill call nothing()
+def nothing(a):
+    pass
+
+def initializeTrackbars(intialTracbarVals,wT=480, hT=240):
+    cv2.namedWindow("Trackbars")
+    cv2.resizeWindow("Trackbars", 360, 240)
+    cv2.createTrackbar("Width Top", "Trackbars", intialTracbarVals[0],wT//2, nothing)
+    cv2.createTrackbar("Height Top", "Trackbars", intialTracbarVals[1], hT, nothing)
+    cv2.createTrackbar("Width Bottom", "Trackbars", intialTracbarVals[2],wT//2, nothing)
+    cv2.createTrackbar("Height Bottom", "Trackbars", intialTracbarVals[3], hT, nothing)
+
+def valTrackbars(wT=480, hT=240):
+    widthTop = cv2.getTrackbarPos("Width Top", "Trackbars")
+    heightTop = cv2.getTrackbarPos("Height Top", "Trackbars")
+    widthBottom = cv2.getTrackbarPos("Width Bottom", "Trackbars")
+    heightBottom = cv2.getTrackbarPos("Height Bottom", "Trackbars")
+    points = np.float32([(widthTop, heightTop), (wT-widthTop, heightTop),
+                      (widthBottom , heightBottom ), (wT-widthBottom, heightBottom)])
+    return points
